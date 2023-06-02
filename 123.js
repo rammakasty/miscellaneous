6f1ce0a3e48e613f32d1ef17b076f19a000f9bf5
choi ha young <gkdudk15@naver.com>
2023-06-02 11:21:07 +0900

    소수점 반올림해서 정수로 변환

--- 123.js
+++ 123.js
@@ -7,5 +7,11 @@ let b = 605*3
 console.log(b);
 
 
-console.log(17320/a)
-console.log(9990/b);
+let c = 17320/a
+let d = 9990/b
+
+console.log(c);
+console.log(d);
+
+console.log(Math.round(c));
+console.log(Math.round(d));
